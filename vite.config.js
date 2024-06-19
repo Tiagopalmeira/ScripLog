d3616ded1e3cde26dd75c8f939ad56998e682d7c
darkwesy <thiagopazba@gmail.com>
2024-06-19 10:00:10 -0300

    Adicionar plugin de .env do vite

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    ‘process.env.VITE_API_URL’:JSON.stringify(process.env.VITE_API_URL),
-  },
+  plugins: [
+    react(),
+    envCompatible({
+      prefix: 'VITE_',
+    }),
+  ],
 });
