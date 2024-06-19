b35bc2fc8dd95a1b3c1b044768969ced84f1630c
darkwesy <thiagopazba@gmail.com>
2024-06-19 09:54:45 -0300

    Ajustar configuraçao do vite para .env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    ‘process.env.VITE_API_URL’:JSON.stringify(process.env.VITE_API_URL),
+  },
+});
